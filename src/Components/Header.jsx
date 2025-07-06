0693a3a8ceea9e198401c8b5feec16f01f2bf31e
Zubain-Nadeem <Zubainali23@gmail.com>
2025-07-06 09:58:11 +0500

    Header created

---
+++ src/Components/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import LogoImage from "../assets/images/header-images/Header-logo.png"
+import CartImage from "../assets/images/header-images/cart.png"
+
+const Header = () => {
+    return (
+        <div>
+            <header className='border border-black w-[100%] h-[75px] flex items-center'>
+                <section className='w-[60%] h-[75px] px-6 flex items-center bg-linear-to-r from-[#9EC55F] to-[#C5C032] justify-between'>
+                    <img src={LogoImage} alt="Logo images" className='h-8' />
+
+                    <nav className='flex list-none  gap-[45px] text-xl font-semibold text-[#42442B]'>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Gallery</a></li>
+                        <li><a href="#">Artists</a></li>
+                        <li><a href="#">FAQ</a></li>
+                    </nav>
+                </section>
+
+
+                <section className='border border-yellow-800 w-[40%] h-[75px] bg-[#030303] text-white flex items-center justify-center gap-11'>
+                    <img src={CartImage} alt="cart image" className='h-9' />
+                    <button className='bg-[#FFA600] px-4 py-2 cursor-pointer rounded-3xl'>GET STARTED</button>
+                </section>
+
+            </header>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='border-4 border-black w-full h-[100vh]'>
+
+        <section className='border border-red-700 w-[60%] min-h-[650px]  bg-linear-to-bl from-[#F2EC3B] to-[#A1F69A] flex flex-col justify-center items-center'>
+                <div className='border border-black w-[70%] p-6'>
+                    <h1 className='text-7xl font-bold '>Cartoonize Yourself</h1>
+                </div>
+        </section>
+      
+    </div>
+  )
+}
+
+export default Hero
